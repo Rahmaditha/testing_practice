5f3afa7c9049679d1e32c1623af894843e5ea48f
Rahmaditha <ditharahma12@gmail.com>
2023-11-02 17:13:26 +0700

    adding serviceLocator

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import org.junit.Before
+
+internal class TasksViewModelTest{
+
+    private lateinit var tasksRepository: FakeTestRepository
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel(){
+        // We initialise the tasks to 3, with one active and two completed
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
+
+    }
+}
+
